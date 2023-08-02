651f2a88e13b6e20c3e2937bc1c4002b701a5637
fcolic <fcolic8920rn@raf.rs>
2023-08-03 00:11:42 +0200

    More init data.

--- scripts/init-data.js
+++ scripts/init-data.js
@@ -5,6 +5,8 @@ db.users.insertOne({
     firstname: "Minad",
     lastname: "Adminovic",
     role: "ADMIN",
+    canComment: false,
+    takeBook: false
 });
 
 db.users.insertOne({
@@ -13,7 +15,9 @@ db.users.insertOne({
     email: "lib@email.com",
     firstname: "Lib",
     lastname: "Raryan",
-    role: "LIBRARIAN"
+    role: "LIBRARIAN",
+    canComment: false,
+    takeBook: false
 });
 
 db.users.insertOne({
@@ -22,7 +26,89 @@ db.users.insertOne({
     email: "reg@email.com",
     firstname: "Reginald",
     lastname: "Userland",
-    role: "REGULAR"
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "reg1",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "reg1@email.com",
+    firstname: "Reginald1",
+    lastname: "Userland1",
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "regualrUser2",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "reg2@email.com",
+    firstname: "Reginald2",
+    lastname: "Userland2",
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "regualrUser3",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "reg3@email.com",
+    firstname: "Reginald3",
+    lastname: "Userland3",
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "regualrUser4",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "reg4@email.com",
+    firstname: "Reginald4",
+    lastname: "Userland4",
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "regualrUser5",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "reg5@email.com",
+    firstname: "Reginald5",
+    lastname: "Userland5",
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "regualrUser6",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "reg6@email.com",
+    firstname: "Reginald6",
+    lastname: "Userland6",
+    role: "REGULAR",
+    canComment: true,
+    takeBook: true
+});
+db.users.insertOne({
+    username: "lib2",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "lib2@email.com",
+    firstname: "Lib2",
+    lastname: "Raryan2",
+    role: "LIBRARIAN",
+    canComment: false,
+    takeBook: false
+});
+db.users.insertOne({
+    username: "lib3",
+    password: "$2b$10$kgfanuzauAUAhhCYL1wOe.q/1uA9G/9Yls1p/V8gE3wNKRIKi1ysm",
+    email: "lib3@email.com",
+    firstname: "Lib3",
+    lastname: "Raryan3",
+    role: "LIBRARIAN",
+    canComment: false,
+    takeBook: false
 });
 
 db.createUser(
