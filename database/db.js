4239b2ab1a502832b8e08389990979bcb75682c2
fcolic89 <filip.colic00@gmail.com>
2024-02-16 16:03:19 +0100

    db.js refactor

--- database/db.js
+++ database/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27017/library?directConnection=true&authSource=library&replicaSet=replica-set-name&retryWrites=true'
+const uri = process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27017/library?directConnection=true&authSource=library&replicaSet=replica-set-name&retryWrites=true';
 
-mongoose.connect(uri, {useUnifiedTopology: true});
+mongoose.connect(uri, { useUnifiedTopology: true });
 
 const con = mongoose.connection;
-con.on('error', (err) => console.error('Could not connect to MongoDB, ' + err));
+con.on('error', (err) => console.error(`Could not connect to MongoDB, ${err}`));
 con.once('open', () => console.log('Connected to MongoDB...'));
 
-process.on('exit', function(){
-    mongoose.disconnect();
+process.on('exit', () => {
+  mongoose.disconnect();
 });
 
 module.exports = con;
