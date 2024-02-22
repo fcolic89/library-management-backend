9892fb2e7765c7f072b0f1b4d6d679b042a17edc
fcolic89 <filip.colic00@gmail.com>
2024-02-22 02:46:40 +0100

    moved generateToken function to lib/misc.js, auth service error fix

--- service/auth.js
+++ service/auth.js
@@ -1,34 +1,30 @@
-const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { User } = require('../database/models');
-
-const privateKey = process.env.PRIVATE_KEY || 'supersecretandsuperprivatekey';
+const error = require('../middleware/errorHandling/errorConstants');
+const { generateToken } = require('../lib/misc');
 
 const login = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).json({ message: 'Invalid email or password!' });
+  const { email } = req.body;
+
+  const user = await User.findOne({ email }).lean();
+  if (!user) {
+    throw new Error(error.NOT_FOUND);
+  }
+
   const match = await bcrypt.compare(req.body.password, user.password);
+  if (!match) {
+    throw new Error(error.CREDENTIALS_ERROR);
+  }
 
-  if (!match) return res.status(400).json({ message: 'Invalid email or password!' });
-
-  const token = jwt.sign({
-    id: user.id,
-    username: user.username,
-    role: user.role,
-    canComment: user.canComment,
-    takeBook: user.takeBook,
-  }, privateKey);
-  res.json({ jwt: token });
+  const token = generateToken(
+    user._id,
+    user.username,
+    user.role,
+    user.canComment,
+    user.takeBook,
+  );
+  return res.json({ jwt: token });
 };
-
-const generateToken = (id, username, role, canComment, takeBook) => (
-  jwt.sign({
-    id,
-    username,
-    role,
-    canComment,
-    takeBook,
-  }, privateKey));
 
 module.exports = {
   login,
